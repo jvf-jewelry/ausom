e01ccdced11547c45ba2825e6c8e35088ddf3985
montanam <montanarimhl@gmail.com>
2019-10-11 11:25:51 +0200

    Test inapp browser

--- www/js/index.js
+++ www/js/index.js
@@ -2480,7 +2480,7 @@ app.controller("JvfController", function($scope, $route, $rootScope, $http, $coo
   }
 
   ctl.register = function(){
-    window.open("https://www.jewelryvirtualfair.com/en/", '_system');
+    window.open("https://www.jewelryvirtualfair.com/en/", '_blank');
   }
 
 
