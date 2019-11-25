b7b11df267c00e8b8e78ca1eb130a0ee34d06717
montanam <montanarimhl@gmail.com>
2019-11-25 16:31:27 +0100

    Test 2 scelta dinamica lingua

--- www/js/index.js
+++ www/js/index.js
@@ -2141,25 +2141,25 @@ app.controller("JvfController", function($scope, $route, $rootScope, $http, $coo
 
   console.log("USER LANGUAGE => " +  window.navigator.userLanguage);
   console.log("LANGUAGE E BASTA => " +  window.navigator.language);
-  // localStorage.lang = window.navigator.userLanguage || window.navigator.language;
-  var lang;
+  localStorage.lang = window.navigator.userLanguage || window.navigator.language;
+  // var lang;
 
-  if (navigator && navigator.userAgent && (lang = navigator.userAgent.match(/android.*\W(\w\w)-(\w\w)\W/i))) {
-    lang = lang[1];
-  }
-  if (!lang && navigator) {
-    if (navigator.language) {
-      lang = navigator.language;
-    } else if (navigator.browserLanguage) {
-      lang = navigator.browserLanguage;
-    } else if (navigator.systemLanguage) {
-      lang = navigator.systemLanguage;
-    } else if (navigator.userLanguage) {
-      lang = navigator.userLanguage;
-    }
-    lang = lang.substr(0, 2);
-  }
-  localStorage.lang = lang
+  // if (navigator && navigator.userAgent && (lang = navigator.userAgent.match(/android.*\W(\w\w)-(\w\w)\W/i))) {
+  //   lang = lang[1];
+  // }
+  // if (!lang && navigator) {
+  //   if (navigator.language) {
+  //     lang = navigator.language;
+  //   } else if (navigator.browserLanguage) {
+  //     lang = navigator.browserLanguage;
+  //   } else if (navigator.systemLanguage) {
+  //     lang = navigator.systemLanguage;
+  //   } else if (navigator.userLanguage) {
+  //     lang = navigator.userLanguage;
+  //   }
+  //   lang = lang.substr(0, 2);
+  // }
+  // localStorage.lang = lang
 
   ctl.apply = function(){
     setTimeout(function(){ $scope.$apply();}, 50);
