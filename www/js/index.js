2b2835cf461323210b180c440f81f3541305a94c
montanam <montanarimhl@gmail.com>
2019-11-26 14:21:03 +0100

    Grafica chat nei dettagli

--- www/js/index.js
+++ www/js/index.js
@@ -2138,24 +2138,6 @@ app.controller("JvfController", function($scope, $route, $rootScope, $http, $coo
   ctl.APPVERSION = '2.1';
   ctl.is_cordova = !!window.cordova;
   app.is_cordova = !!window.cordova;
-  // var lang;
-
-  // if (navigator && navigator.userAgent && (lang = navigator.userAgent.match(/android.*\W(\w\w)-(\w\w)\W/i))) {
-  //   lang = lang[1];
-  // }
-  // if (!lang && navigator) {
-  //   if (navigator.language) {
-  //     lang = navigator.language;
-  //   } else if (navigator.browserLanguage) {
-  //     lang = navigator.browserLanguage;
-  //   } else if (navigator.systemLanguage) {
-  //     lang = navigator.systemLanguage;
-  //   } else if (navigator.userLanguage) {
-  //     lang = navigator.userLanguage;
-  //   }
-  //   lang = lang.substr(0, 2);
-  // }
-  // localStorage.lang = lang
 
   ctl.apply = function(){
     setTimeout(function(){ $scope.$apply();}, 50);
